f65dbd5cee92bb32495c8db5a71c741996c8a6cb
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-02-21 18:20:11 +0530

    prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+/*
+* Decorators -> addition of metadata and additional behaviors to classes,
+    methods, properties, and function parameters
+    eg: @Module
+*/
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookmarkModule],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  //signup functionality
+  signup() {
+    return 'signin function';
+  }
+  //login functionality
+  signin() {
+    return 'signin function';
+  }
+}
